c0d4d5bd9a482a150f4b3340d06b9c35ed08eeff
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-04 15:14:18 +0300

    refactor: update the file structure in explore-nearby section to separate ui components for clean work

---
+++ src/app/_components/ExploreNearby/ExploreNearbyCard.tsx
@@ -0,0 +1,34 @@
+import { exploreNearby } from "@/types/exploreNearby";
+import { Heart } from "lucide-react";
+import Image from "next/image";
+
+const ExploreNearbyCard = ({ item }: { item: exploreNearby }) => {
+  return (
+    <div>
+      <div className="relative aspect-square w-full overflow-hidden rounded-xl cursor-pointer group">
+        <Image
+          src={item.img}
+          alt={item.location}
+          fill
+          sizes="(max-width: 768px) 50vw, 25vw"
+          className="object-cover transition-transform duration-300 group-hover:scale-110"
+        />
+
+        {item.tag && (
+          <span className="absolute z-50 inset-s-3 top-3 text-xs rounded-full bg-white py-1 px-2.5 font-medium">
+            {item.tag}
+          </span>
+        )}
+
+        <Heart className="absolute z-50 inset-e-3 top-3 w-6 h-6 text-white fill-secondary transition-transform duration-300 hover:scale-110" />
+      </div>
+
+      <div className="mt-2 ps-1.5 text-sm font-medium">
+        <p>{item.location}</p>
+        <span className="text-secondary">{item.distance}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ExploreNearbyCard;
--- src/app/_components/ExploreNearby/index.tsx
+++ src/app/_components/ExploreNearby/index.tsx
@@ -1,7 +1,6 @@
 import MainHeading from "@/components/MainHeading";
 import { exploreNearby } from "@/types/exploreNearby";
-import { Heart } from "lucide-react";
-import Image from "next/image";
+import ExploreNearbyCard from "./ExploreNearbyCard";
 
 export const exploreData: exploreNearby[] = [
   {
@@ -68,30 +67,7 @@ const ExploreNearby = () => {
 
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
           {exploreData.map((item: exploreNearby) => (
-            <div key={item.id}>
-              <div className="relative aspect-square w-full overflow-hidden rounded-xl cursor-pointer group">
-                <Image
-                  src={item.img}
-                  alt={item.location}
-                  fill
-                  sizes="(max-width: 768px) 50vw, 25vw"
-                  className="object-cover transition-transform duration-300 group-hover:scale-110"
-                />
-
-                {item.tag && (
-                  <span className="absolute z-50 inset-s-3 top-3 text-xs rounded-full bg-white py-1 px-2.5 font-medium">
-                    {item.tag}
-                  </span>
-                )}
-
-                <Heart className="absolute z-50 inset-e-3 top-3 w-6 h-6 text-white fill-secondary transition-transform duration-300 hover:scale-110" />
-              </div>
-
-              <div className="mt-2 ps-1.5 text-sm font-medium">
-                <p>{item.location}</p>
-                <span className="text-secondary">{item.distance}</span>
-              </div>
-            </div>
+            <ExploreNearbyCard key={item.id} item={item} />
           ))}
         </div>
       </div>
